06c48a471e59fcf2ffae5027f93268a754bab1ed
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2023-12-12 19:37:59 -0300

    feature: implementa assets(sizes e weights)

---
+++ src/Presentation/assets/sizes/sizes.ts
@@ -0,0 +1,5 @@
+export enum Sizes {
+  small = 14,
+  medium = 20,
+  large = 26,
+}
---
+++ src/Presentation/assets/weights/weights.ts
@@ -0,0 +1,7 @@
+export enum Weights {
+  extraTiny = '100',
+  tiny = '200',
+  normal = '400',
+  bold = '600',
+  extraBold = '900',
+}
