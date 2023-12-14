62c4d56e040d7d1bc9c71fe168d9660ac6ccb3b1
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2023-12-14 09:30:47 -0300

    ajusta alinhamento da screen

--- src/Presentation/components/Screen.styled.ts
+++ src/Presentation/components/Screen.styled.ts
@@ -4,4 +4,6 @@ import {Colors} from '../assets/colors/colors';
 export const ScreenStyled = styled.SafeAreaView`
   flex: 1;
   background-color: ${Colors.background};
+  justify-content: center;
+  align-items: center;
 `;
